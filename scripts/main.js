8986ad7e17b4a157ee43e99c28681ca468ab4421
davidrodjen <davidrodriguezjenkins@gmail.com>
2019-11-14 14:39:11 -0800

    Made adjustments for array, local storage utilization

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,32 @@
 window.onload = function () {
     initBuyButtons();
+    //display the number at the start
+    displayNumberOfItems();
+    var cartIcon = document.querySelector("#shopping-cart");
+    cartIcon.onclick = showCartContents;
 };
+function showCartContents() {
+    var displayDiv = document.querySelector("#display-cart");
+    displayDiv.innerHTML = ""; //this makes it so multiple clicks of the shopping cart does not repeatedly add the cart
+    var allProds = ProductStorage.getAllProducts();
+    //programatically create a structure for each product that makes it able to be manipulated by CSS
+    for (var i = 0; i < allProds.length; i++) {
+        var prod = allProds[i];
+        /*((recreate this with the for loop programmatically))
+            <div class="display-product">
+                <h2>Widget - $80.00</h2>
+                <p>Widgets are really cool</p>
+            </div>
+        */
+        var prodDiv = document.createElement("div");
+        prodDiv.classList.add("display-product");
+        var h2 = document.createElement("h2");
+        h2.innerHTML = prod.title + " - $" + prod.price;
+        //h2.innerHTML = `${prod.title} - $${prod.price}`; Another way of doing it, template literal
+        prodDiv.appendChild(h2);
+        displayDiv.appendChild(prodDiv);
+    }
+}
 /**
  * Wire up all the "Buy" buttons to call buyProduct
  */
@@ -12,11 +38,20 @@ function initBuyButtons() {
     }
 }
 function buyProduct() {
-    var prod = getProduct();
+    var currBtn = this; // The "Buy" button that was clicked
+    var prod = getProduct(currBtn);
     saveProductToCart(prod);
+    displayNumberOfItems();
 }
-function getProduct() {
-    var currBuyBtn = this;
+function displayNumberOfItems() {
+    //get number of items in storage
+    var numItems = ProductStorage.getNumberOfProducts();
+    //display it in the span
+    var cartSpan = document.querySelector("div#shopping-cart > span");
+    cartSpan.innerHTML = numItems.toString(); //have to put .toString() because of datatype issues
+}
+function getProduct(currBuyBtn) {
+    console.log("The buy button that was clicked");
     console.log(currBuyBtn);
     //alert("you clicked Buy biotch");
     var currProdDiv = currBuyBtn.parentElement;
@@ -30,10 +65,11 @@ function getProduct() {
     prod.price = parseFloat(price); //needs to be converted to a number
     prod.description = currProdDiv.querySelector("div.description").innerHTML;
     console.log(prod);
-    saveProductToCart(prod);
     return prod;
 }
 function saveProductToCart(p) {
+    ProductStorage.addProduct(p);
+    return ProductStorage.getAllProducts();
 }
 /**
  * Represents a single shopping cart item, field
@@ -48,3 +84,32 @@ var Product = /** @class */ (function () {
 // prod.title = "something";
 // prod.description = "something else";
 // prod.price = 4.99;
+var ProductStorage = /** @class */ (function () {
+    function ProductStorage() {
+    }
+    // Add Product. p as parameter as a Product, Product is the class object we created in the Product TS
+    ProductStorage.addProduct = function (p) {
+        // get existing products
+        var prods = ProductStorage.getAllProducts();
+        prods.push(p);
+        var data = JSON.stringify(prods); //Convert our whole program array to JSON
+        localStorage.setItem("prods", data); //puts it back into local storage
+    };
+    /**
+     * Returns all products or an empty
+     * array if no products are stored
+     */
+    ProductStorage.getAllProducts = function () {
+        var data = localStorage.getItem("prods");
+        if (data == null) {
+            return new Array(); //return array of type product which is the class that we created in the Typescript
+        }
+        return JSON.parse(data); //put the cast of <Product[]> to make sure it casts at the right item
+    };
+    // Get number of Products
+    ProductStorage.getNumberOfProducts = function () {
+        var prods = ProductStorage.getAllProducts();
+        return prods.length;
+    };
+    return ProductStorage;
+}());
--- typescripts/main.ts
+++ typescripts/main.ts
@@ -1,6 +1,43 @@
 window.onload = function(){
     initBuyButtons();
+    //display the number at the start
+    displayNumberOfItems();
+
+    let cartIcon = <HTMLElement>document.querySelector("#shopping-cart");
+    cartIcon.onclick = showCartContents;
 }
+
+function showCartContents(){
+    let displayDiv = document.querySelector("#display-cart");
+
+    displayDiv.innerHTML = ""; //this makes it so multiple clicks of the shopping cart does not repeatedly add the cart
+
+    let allProds = ProductStorage.getAllProducts();
+    //programatically create a structure for each product that makes it able to be manipulated by CSS
+
+    for(let i = 0; i < allProds.length; i++){
+        const prod = allProds[i];
+        /*((recreate this with the for loop programmatically))
+            <div class="display-product">
+                <h2>Widget - $80.00</h2>
+                <p>Widgets are really cool</p>
+            </div>
+        */
+
+       let prodDiv = document.createElement("div");
+       prodDiv.classList.add("display-product");
+
+       let h2 = document.createElement("h2");
+       h2.innerHTML = prod.title + " - $" + prod.price;
+       //h2.innerHTML = `${prod.title} - $${prod.price}`; Another way of doing it, template literal
+
+       prodDiv.appendChild(h2);
+       displayDiv.appendChild(prodDiv);
+
+    }
+}
+
+
 
 /**
  * Wire up all the "Buy" buttons to call buyProduct
@@ -14,16 +51,24 @@ function initBuyButtons() {
 }
 
 function buyProduct(){
-    
-    let prod = getProduct();
+    let currBtn = this; // The "Buy" button that was clicked
+    let prod = getProduct(currBtn);
 
     saveProductToCart(prod);
 
-    }
-    
-function getProduct() {
+    displayNumberOfItems();
+}
 
-    let currBuyBtn = <HTMLElement>this;
+function displayNumberOfItems(){
+    //get number of items in storage
+    let numItems = ProductStorage.getNumberOfProducts();
+    //display it in the span
+    let cartSpan = document.querySelector("div#shopping-cart > span");
+    cartSpan.innerHTML = numItems.toString(); //have to put .toString() because of datatype issues
+}
+    
+function getProduct(currBuyBtn:HTMLElement) {
+    console.log("The buy button that was clicked");
     console.log(currBuyBtn);
     //alert("you clicked Buy biotch");
 
@@ -41,14 +86,10 @@ function getProduct() {
     prod.description = currProdDiv.querySelector("div.description").innerHTML;
     
     console.log(prod);
-    saveProductToCart(prod);
     return prod;
 }
 
-    function saveProductToCart(p:Product):Product[]{
-
-    }
-
-    
-
+function saveProductToCart(p:Product):Product[]{
+    ProductStorage.addProduct(p);
+    return ProductStorage.getAllProducts();
 }
